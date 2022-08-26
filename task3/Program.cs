ba896a3b381b02a43fe8989d3e499552fc99fa3f
Dmitriy B <kupperfeet@gmail.com>
2022-08-26 20:34:32 +0300

    new trext and replace

--- task3/Program.cs
+++ task3/Program.cs
@@ -24,3 +24,9 @@ string Replace(string text, char oldValue, char newValue)
 
 string newText = Replace(text, ' ', '|');
 Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
