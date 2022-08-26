de0ec0f35edbbefe4b4aedd1ca83f9d080050762
Dmitriy B <kupperfeet@gmail.com>
2022-08-26 19:49:04 +0300

    commit FOR or WHILE

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// FOR цикл
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
