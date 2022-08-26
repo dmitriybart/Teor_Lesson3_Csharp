6bade10c634dad6aea56b461db97161c90fc7c5a
Dmitriy B <kupperfeet@gmail.com>
2022-08-26 20:28:33 +0300

    new trext

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// В тексте нужно заменить все пробелы на черточки, маленькие "к" заменить большими, а большие "С" заменить маленькими.
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// sring s = "qwerty"
+//            012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+  string result = String.Empty;
+  int length = text.Length;
+  for (int i = 0; i < length; i++)
+  {
+    if (text[i] == oldValue) result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+  }
+
+  return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
